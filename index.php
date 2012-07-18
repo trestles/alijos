427a1aadbd68aac7babd4fa9424d78fc97ebc75c
Jonathan Twaddell <jon@arclocal.com>
2012-07-18 16:22:43 -0700

    updated

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+
+Alijos is a high-end consulting firm specializing in UNIX development and design. Technologies used include Ruby<br /> 
+on Rails, Zend Framework, Python, MySQL, Objective-C / iOS, and Memcache. Currently, I'm swamped with work but can be reached <a href='mailto:jon@arclocal.com'>here</a>. Thanks
+</body>
+</html>
